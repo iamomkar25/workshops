cf4d06af4c38a32b8db2c1afa04a83ca556151f4
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-04-28 10:05:32 +0530

    1st-workshop

---
+++ Leepyear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leap_year
+{
+    internal class Leepyear
+    {
+        public void year()
+        {
+            Console.WriteLine("Please Enter a Number only 4 digit");
+            int y = Convert.ToInt32(Console.ReadLine());
+            if ((y % 4 == 0))
+            {
+                Console.WriteLine("leepYear");
+
+            }
+            else
+            {
+                Console.WriteLine("not leep year");
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leap_year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Leepyear leep = new Leepyear();
+            leep.year();
+        }
+    }
+    
+}
